1aeb4fcc23776d694d90f5c62e72c238d45f9f50
KamalChauhan1234 <kamalchauhan0037@gmail.com>
2026-01-04 03:38:00 +0530

    Update provider.tf

--- JioCloudInfra/provider.tf
+++ JioCloudInfra/provider.tf
@@ -6,9 +6,9 @@ terraform {
     }
   }
  backend "azurerm" {
-    resource_group_name  = "dev-resource-group"
-    storage_account_name = "devinfrastorage9810"
-    container_name       = "jenkins"
+    resource_group_name  = "rg-dev-001"
+    storage_account_name = "stg001ghhj"
+    container_name       = "dev-container001"
     key                  = "jioinfra.tfstate"
   }
   }
@@ -16,6 +16,7 @@ provider "azurerm" {
   features {}
 subscription_id = "de1c1815-4f90-412b-9551-d55f0de9407d"
 }
+
 
 
 
